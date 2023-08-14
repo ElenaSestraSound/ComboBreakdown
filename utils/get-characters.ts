208c7718327da486fa748a0070b4020a9ade47a0
Arto Lahtinen <artolle@pm.me>
2023-08-14 15:32:27 +0200

    feat: error handling for get characters

--- utils/get-characters.ts
+++ utils/get-characters.ts
@@ -4,6 +4,10 @@ export const revalidate = 60; // 3600 // revalidate at most every hour
 
 // TODO: use react cache to cache results
 export const getCharacters = async () => {
-  let characters = await prisma.character.findMany();
-  return characters;
+  try {
+    let characters = await prisma.character.findMany();
+    return characters;
+  } catch (err) {
+    console.error(err);
+  }
 };
