e00c2f2b5b003b2f2525a5811ed4161cffab0bf9
elena.sestrasound <elena.sestrasound@gmail.com>
2023-08-14 19:46:40 +0200

    chore: added dropdownselector component

---
+++ app/compare/DropdownSelector/DropdownSelector.tsx
@@ -0,0 +1,50 @@
+'use client';
+import * as React from 'react';
+import { MouseEvent, useEffect, useState } from 'react';
+
+type Details = {
+  name: string,
+  index: number;
+};
+
+export type IDropdownSelectorProps = {
+  list: Details[],
+  title: string,
+  onChangeSelection: (selectedIndex: number) => void;
+};
+
+export default function DropdownSelector ({ list, title, onChangeSelection }: IDropdownSelectorProps) {
+  const [selection, setSelection] = useState<Details | null>(null);
+  const [menuShown, setMenuShown] = useState(false);
+
+  const onClickHandler = (e: MouseEvent<HTMLElement>) => {
+    const targetElement = e.target as HTMLElement;
+    const targetId = parseInt(targetElement.id);
+    setSelection({ name: list[targetId].name, index: targetId });
+    setMenuShown(false);
+  };
+
+  useEffect(() => {
+    if (selection) {
+      onChangeSelection(selection.index);
+    }
+  }, [selection]);
+
+  return (
+    <div>
+      <div
+        className='p-2 bg-purple-900'
+        onClick={() => setMenuShown(prev => !prev)}>
+        <span>{selection ? selection.name : title}</span>
+      </div>
+      {menuShown &&
+        <div>
+          {list.map((character, index) => <span key={Date.now() + index}
+            id={index.toString()}
+            onClick={onClickHandler}
+          >{character.name}</span>)}
+        </div>
+      }
+    </div>
+  );
+}
--- app/compare/page.tsx
+++ app/compare/page.tsx
@@ -1,49 +1,79 @@
 "use client";
-import { MouseEvent, useState } from 'react';
+import { MouseEvent, useEffect, useState } from 'react';
 import { characters } from './mockCharacters';
 import { Character, Move } from './types';
+import DropdownSelector from './DropdownSelector/DropdownSelector';
+
 export default function Compare () {
-  const [character, setCharater] = useState<Character | null>(null);
-  const [menuShown, setMenuShown] = useState<boolean>(false);
+  const characterList = listToPairs(characters);
 
-  const [move, setMove] = useState<Move | null>(null);
-  const [moveMenuShown, setMoveMenuShown] = useState<boolean>(false);
+  const [firstCharacter, setFirstCharacter] = useState<Character | null>(null);
+  const [firstCharacterMove, setFirstCharacterMove] = useState<Move | null>(null);
 
-  const onClickHandle = (e: MouseEvent<HTMLElement>) => {
-    const targetElement = e.target as HTMLElement;
-    const targetId = parseInt(targetElement.id);
-    setCharater(characters[targetId]);
-    setMenuShown(false);
+  const [secondCharacter, setSecondCharacter] = useState<Character | null>(null);
+  const [secondCharacterMove, setSecondCharacterMove] = useState<Move | null>(null);
+
+  const onSelectFirstCharacterMove = (index: number) => {
+    setFirstCharacterMove(firstCharacter!.moves[index]);
   };
 
-  const onMoveClickHandle = (e: MouseEvent<HTMLElement>) => {
-    const targetElement = e.target as HTMLElement;
-    const targetId = parseInt(targetElement.id);
-    setMove(character!.moves[targetId]);
-    setMoveMenuShown(false);
+  const onSelectFirstCharacter = (index: number) => {
+    setFirstCharacter(characters[index]);
+    setFirstCharacterMove(null);
+  };
+
+  const onSelectSecondCharacterMove = (index: number) => {
+    setSecondCharacterMove(firstCharacter!.moves[index]);
+  };
+
+  const onSelectSecondCharacter = (index: number) => {
+    setSecondCharacter(characters[index]);
+    setSecondCharacterMove(null);
   };
 
   return (
     <div>
       <div>
-        <div onClick={() => setMenuShown(true)}><span>{character ? character.name : 'SELECT A CHARACTER'}</span></div>
-        {menuShown &&
-          <div>
-            {characters.map((character, index) => <span
-              id={index.toString()}
-              onClick={onClickHandle}
-            >{character.name}</span>)}
-          </div>}
-        {character && <div onClick={() => setMoveMenuShown(true)}><span>{move ? move.name : 'SELECT A MOVE'}</span></div>}
-        {moveMenuShown && character?.moves.map((move, index) => <div
-          id={index.toString()}
-          onClick={onMoveClickHandle}
-        >{move.name}</div>)}
+        <DropdownSelector
+          list={characterList}
+          title={'SELECT A CHARACTER'}
+          onChangeSelection={onSelectFirstCharacter} />
+        {firstCharacter &&
+          <DropdownSelector
+            // by adding this key the component knows that the caracter selection
+            // has changed and it resets to its default value
+            key={firstCharacter.name}
+            list={listToPairs(firstCharacter.moves)}
+            title={'SELECT MOVE'}
+            onChangeSelection={onSelectFirstCharacterMove} />}
       </div>
-      {move &&
-        <div>
-          {move.frameData.startup} frames on startup
-        </div>}
+      <div>
+        VS
+      </div>
+      <div>
+        <DropdownSelector
+          list={characterList}
+          title={'SELECT A CHARACTER'}
+          onChangeSelection={onSelectSecondCharacter} />
+        {secondCharacter &&
+          <DropdownSelector
+            // by adding this key the component knows that the caracter selection
+            // has changed and it resets to its default value
+            key={secondCharacter.name}
+            list={listToPairs(secondCharacter.moves)}
+            title={'SELECT MOVE'}
+            onChangeSelection={onSelectSecondCharacterMove} />}
+      </div>
+
     </div>
   );
 }
+
+const listToPairs = (list: any[]) => {
+  return list.map((elem, index) => {
+    return {
+      name: elem.name as string,
+      index
+    };
+  });
+};
