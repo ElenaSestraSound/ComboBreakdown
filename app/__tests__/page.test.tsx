7635b64b7408585d2f25217f60492c6e16d39031
Arto Lahtinen <artolle@pm.me>
2023-08-15 12:22:52 +0200

    feat: improve get characters and tests

--- app/__tests__/page.test.tsx
+++ app/__tests__/page.test.tsx
@@ -2,6 +2,17 @@ import { render, screen } from '@testing-library/react';
 import Page from '../page';
 
 jest.mock('../main/character-list');
+jest.mock("react", () => ({
+  ...jest.requireActual("react"),
+  use: jest.fn(() => { })
+}));
+jest.mock('../../utils/get-characters', () => ({
+  getCharacters: () => jest.fn(() => { })
+})
+);
+afterEach(() => {
+  jest.clearAllMocks();
+});
 
 describe('Root Page', () => {
   it('renders heading', async () => {
--- utils/get-characters.ts
+++ utils/get-characters.ts
@@ -1,13 +1,13 @@
+import { cache } from 'react';
 import { prisma } from '@/lib/prisma';
 
 export const revalidate = 60; // 3600 // revalidate at most every hour
 
-// TODO: use react cache to cache results
-export const getCharacters = async () => {
+export const getCharacters = cache(async () => {
   try {
     let characters = await prisma.character.findMany();
     return characters;
   } catch (err) {
     console.error(err);
   }
-};
+});
