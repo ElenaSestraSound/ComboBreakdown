2ee4449987184c3f6616a04e8f3deebce8911124
Arto Lahtinen <artolle@pm.me>
2023-08-16 12:37:41 +0200

    feat: nav-bar layout v1

--- app/header/header.tsx
+++ app/header/header.tsx
@@ -3,8 +3,8 @@ import NavBar from "./header-nav";
 
 export default function Header () {
   return (
-    <header className="grid grid-cols-2 p-2">
-      <Link href="/"><h1>Combo Breakdown</h1></Link>
+    <header className="grid grid-cols-2 p-4">
+      <Link href="/"><h1 className="text-xl uppercase leading-4">Combo <span className="block tracking-tighter leading-5 italic text-lg mx-2">Breakdown</span></h1></Link>
       <NavBar></NavBar>
     </header>
   );
