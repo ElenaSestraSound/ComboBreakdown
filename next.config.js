fda911dafb9ba433a4839adf1225dee15374296a
elena.sestrasound <elena.sestrasound@gmail.com>
2023-08-18 17:14:32 +0200

    fix: set path for next images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['combo-breakdown-dev.vercel.app', 'combo-breakdown.vercel.app'],
+    path: "/"
   },
 };
 
