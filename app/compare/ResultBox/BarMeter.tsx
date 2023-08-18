b884f558659bd0d706da3c056defc8805e170380
Arto Lahtinen <artolle@pm.me>
2023-08-18 10:28:05 +0200

    feat: stacked bar for the compare page (#10)

--- app/compare/ResultBox/BarMeter.tsx
+++ app/compare/ResultBox/BarMeter.tsx
@@ -1,15 +1,78 @@
-import * as React from 'react';
-
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import { options } from '@/utils/bar-options';
 export interface IBarMeterProps {
   startup: number,
   active: string,
   recovery: number;
 }
 
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
 export function BarMeter ({ startup, active, recovery }: IBarMeterProps) {
+  const activeLast = parseInt(active.split('-')[1]);
+  const activeVal = activeLast - startup;
+  const recoveryVal = recovery - activeLast;
+  const max = 100 - startup - activeVal - recoveryVal;
+
+  const data = {
+    labels: ['bar'],
+    datasets: [{
+      label: 'Startup',
+      data: [startup],
+      backgroundColor: [
+        'rgba(60, 230, 60, 0.85)',
+      ],
+      stack: 'bar',
+    }, {
+      label: 'Active',
+      data: [activeVal],
+      backgroundColor: [
+        'rgba(230, 60, 60, 0.85)',
+      ],
+      stack: 'bar'
+    }, {
+      label: 'Recovery',
+      data: [recoveryVal],
+      backgroundColor: [
+        'rgba(80, 80, 230, 0.85)',
+      ],
+      stack: 'bar'
+    }, {
+      label: '-',
+      data: [max],
+      backgroundColor: [
+        'rgba(35, 15, 55, 1)',
+      ],
+      stack: 'bar',
+      legend: { display: false }
+    }
+    ]
+  };
+
   return (
     <div className='w-full flex justify-center'>
-      {/* TODO Fill with new chart library */}
+      <Bar
+        data={data}
+        height={90}
+        width={`100%`}
+        options={options}
+      />
     </div >
   );
 }
