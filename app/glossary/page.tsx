26faaa7ffdfa013951fe5ff08f22bf1c3ea9a87c
Arto Lahtinen <artolle@pm.me>
2023-08-21 15:07:33 +0200

    style: glossary responsiveness and polish (#21)

--- app/glossary/page.tsx
+++ app/glossary/page.tsx
@@ -53,8 +53,8 @@ export default function Glossary () {
   }, [currentPage, filteredData]);
 
   return (
-    <div className="max-w-4xl mx-auto  glossary-page bg-[url('/characterpage/juri.png')]">
-      <div className="overlay h-full w-full py-10 px-10 md:px-20">
+    <div className="max-w-4xl mx-auto h-full glossary-page bg-[url('/characterpage/juri.png')]">
+      <div className="overlay h-full w-full py-10 lg:px-10 md:px-20 px-10">
         <div>
           <form>
             <label htmlFor="glossary-filter">Search Glossary</label>
