009e328858439dccef69e5449a4c5b9461d54cc7
elena.sestrasound <elena.sestrasound@gmail.com>
2023-08-22 12:54:43 +0200

    test: compare component and dynamic background tests

---
+++ app/compare/DynamicBackground/dynamicBackground.test.tsx
@@ -0,0 +1,34 @@
+import { act, render } from "@testing-library/react";
+import { DynamicBackground } from "./DynamicBackground";
+
+
+const altTextLeft = 'An image of Luke';
+const altTextRight = 'An image of Ken';
+
+describe('Dynamic Background', () => {
+  it('should not show any images if no character is selected', async () => {
+    await act(async () => {
+      render(<DynamicBackground />);
+    });
+    const leftBackgroundElement = document.querySelector('[data-testid="bg-left"]');
+    const rightBackgroundElement = document.querySelector('[data-testid="bg-right"]');
+
+    expect(leftBackgroundElement).not.toBeInTheDocument();
+    expect(rightBackgroundElement).not.toBeInTheDocument();
+  });
+  it('should show character images when a character is selected', async () => {
+    const getByTestId = await act(async () => {
+      const { getByTestId } = render(<DynamicBackground left="Luke" right="Ken" />);
+      return getByTestId;
+    });
+
+    const leftBackgroundElement = getByTestId('bg-left');
+    const rightBackgroundElement = getByTestId('bg-right');
+
+    const leftStyles = window.getComputedStyle(leftBackgroundElement);
+    const rightStyles = window.getComputedStyle(rightBackgroundElement);
+
+    expect(leftStyles.getPropertyValue('background-image')).toContain('/dynamic/left/luke.png');
+    expect(rightStyles.getPropertyValue('background-image')).toContain('/dynamic/right/ken.png');
+  });
+});
--- app/compare/compare.test.tsx
+++ app/compare/compare.test.tsx
@@ -2,12 +2,21 @@ import { act, render, screen } from '@testing-library/react';
 import Compare from './Compare';
 import { characters } from './mocks/mockCharacters';
 
-describe('Compare Page', () => {
+describe('Compare', () => {
   it('should render 2 character selectors', async () => {
     await act(async () => {
       render(<Compare characters={characters} />);
     });
     const compareText = screen.getAllByText(/SELECT A CHARACTER/i);
     expect(compareText.length).toBe(2);
+  });
+  it('should render the VS image', async () => {
+    const altText = 'An image of VS letters';
+    const src = '/common/vs.png';
+    await act(async () => {
+      render(<Compare characters={characters} />);
+    });
+    const imageElement = screen.getByAltText(altText);
+    expect(imageElement).toBeInTheDocument();
   });
 });
