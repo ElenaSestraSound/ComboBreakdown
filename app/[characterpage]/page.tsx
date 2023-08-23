c3ac2743e1458eb2f10245808dd3c1db18f58dbc
Chris Sorensen <chris_sorensen@live.com>
2023-08-23 11:12:09 +0200

    rewrite

--- app/[characterpage]/page.tsx
+++ app/[characterpage]/page.tsx
@@ -9,12 +9,16 @@ export default function CharacterPage({ searchParams }: ICharacterPage) {
   const charObj = use(getCharacters()); // Get data from app service
   const charDisp = charObj?.filter(x => x.name === searchParams.charName);
   const bckURL = `'/characterpage/${searchParams.charName}.png'`;
+    const prefix: string = '';
 
   return (
-    <div className="character-page h-full flex flex-row justify-around relative" style={{ backgroundImage: `url(${bckURL})` }}>
-      {/* tailwind prop: opacity-10 */}
-      <div className="overlay h-full flex flex-row justify-around">
-        <div id="bio" className="p-24 w-2/5 sticky">
+    // <div className="character-page h-full block lg:flex lg:flex-row items-center lg:justify-around relative" style={{ backgroundImage: `url(${bckURL})` }}>
+    //   {/* tailwind prop: opacity-10 */}
+    //   <div className="overlay w-full h-full block lg:flex lg:flex-row lg:justify-around">
+      <div className="character-page h-full block lg:flex lg:justify-center relative" 
+    style={{ backgroundImage: `url(${bckURL})` }}>
+    <div className="overlay w-full h-full lg:flex justify-center block lg:flex-row lg:justify-around lg:sticky pos-0">
+        <div id="bio" className="shrink-0 p-24 max-w-xl ">
           <h2 className="text-4xl">{charDisp ? charDisp[0]['name'].toUpperCase() : 'CharTitle'}</h2>
           <p>{charDisp ? charDisp[0]['height'] : 'CharHeight'}</p>
           <p>{charDisp ? charDisp[0]['weight'] : 'CharWeight'}</p>
@@ -22,7 +26,7 @@ export default function CharacterPage({ searchParams }: ICharacterPage) {
           <p>{charDisp ? charDisp[0]['notlike'] : 'CharNotlike'}</p>
           <p>{charDisp ? charDisp[0]['bio'] : 'CharBio'}</p>
         </div>
-        <div id="moves" className="p-24 static">
+        <div id="moves" className="p-24 static ">
           <div id="controller-options" className=" w-96 shadow-inner bg-gradient-to-r from-purple-950 to-indigo-900 p-3 px-4 flex justify-around rounded">
             <div>
               <button title='1' className="" type="button"><Image height={10} width={100} src="/moveBtn/logo-classic.png" alt="Classic"></Image></button>
@@ -35,12 +39,12 @@ export default function CharacterPage({ searchParams }: ICharacterPage) {
             {
               charDisp &&
               charDisp[0]['moves'].map((move) => (
-                  <MoveElem
-                  key={move.id}
-                  move = {move}
-                  />
-              
-              ))}
+                <MoveElem
+                key={move.id}
+                move={move}
+                prefix={prefix}
+                />
+            ))}
           </ul>
         </div>
       </div>
--- app/main/Move/move.tsx
+++ app/main/Move/move.tsx
@@ -1,38 +1,59 @@
 
 
-export default function MoveElem(params:any) {
+export default function MoveElem(params: any) {
   const move = params.move;
-  let first: string = '';
-  let second: string = '';
-  if (!move) return 'I aint got no moves man!';
+  const prefix = params.prefix;
+  const btnUrls: string[] = [];
 
-  if (move.classic) {
-    first = move.classic[1];
-    second = move.classic[0];
-    first ? first = first.toLowerCase() : first;
-    second ? second = second.toLowerCase() : second;
+  if (!move) {
+    return 'I aint got no moves man!';
+  } else if (move.type === 'normal') {
+    return;
+  } else if (move.classic === 'NO INPUT' || move.classic === 'default') {
+    // btnUrls.push('blank');
+    return;
+  } else {
+    let curr = move.classic;
+    curr = curr.toLowerCase();
+    for (let i = 0; i < curr.length; i++) {
+      let temp = curr[i];
+      if (temp === 'l' || temp === 'm' || temp === 'h') {
+        btnUrls.push(curr[i + 1] + curr[i]);
+        i++;
+      } else if (temp === '|') {
+        btnUrls.push('or');
+      } else if (temp === '+') {
+        btnUrls.push('plus');
+      } else if (temp === '_') {
+        btnUrls.push('a');
+      } else {
+        btnUrls.push(temp);
+      }
+    }
+    // console.log(btnUrls);
   }
 
   return (
     <li key={move.id}>
-      <div className="flex w-96 items-center p-3 bg-gradient-to-r from-purple-950 to-indigo-900 my-5 rounded shadow-inner">
+      <div className="flex max-w-md items-center p-3 bg-gradient-to-r from-purple-950 to-indigo-900 my-5 rounded shadow-inner">
         <div
-          style={{ backgroundImage: `url("/moves/101.jpg")` }}
-          className="h-10 w-16 bg-cover bg-center rounded shadow-md"
+          style={{ backgroundImage: `url("/moves/403.jpg")` }}
+          className="h-10 bg-cover bg-center rounded shadow-md"
         ></div>
         <div className="text-center pl-3">
           <p>{move.name}</p>
           <div className="flex px-5 py-1 items-center">
-            <span>
-              <div
-                className="h-6 w-6 z-10 bg-cover"
-                style={{ backgroundImage: `url("/moveBtn/icon_${first + second}.png")` }}
-              />
-            </span>
-            <p className="px-2 text-xs">{move.classic[0]}</p>
+            {btnUrls &&
+              btnUrls.map((bUrl, index) => (
+                <span
+                  key={move.id.toString() + index}
+                  className="h-6 w-6 z-10 bg-cover flex"
+                  style={{ backgroundImage: `url('/moveBtn/icon_${prefix}${bUrl}.png')` }}>
+                </span>
+              ))}
           </div>
         </div>
       </div>
-    </li >
+    </li>
   )
 }
