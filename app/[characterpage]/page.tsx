7591ed4e8bea181f5bc4e4718b5fb5c8aae25546
Chris Sorensen <chris_sorensen@live.com>
2023-08-24 09:53:45 +0200

    small commit

--- app/[characterpage]/page.tsx
+++ app/[characterpage]/page.tsx
@@ -1,6 +1,5 @@
 import { use } from 'react';
 import { getCharacters } from '@/utils/get-characters';
-import Image from 'next/image';
 import MoveElem from '@/app/main/Move/move';
 import { ICharacterPage } from "@/utils/types";
 
@@ -11,10 +10,10 @@ export default function CharacterPage({ searchParams }: ICharacterPage) {
   const bckURL = `'/characterpage/${searchParams.charName}.png'`;
 
   return (
-    <div className="character-page h-full flex flex-row justify-around relative" style={{ backgroundImage: `url(${bckURL})` }}>
-      {/* tailwind prop: opacity-10 */}
-      <div className="overlay h-full flex flex-row justify-around">
-        <div id="bio" className="p-24 w-2/5 sticky">
+    <div className="character-page max-w-4xl mx-auto h-full" 
+    style={{ backgroundImage: `url(${bckURL})` }}>
+      <div className="overlay w-full h-full lg:flex justify-center content-center lg:flex-row lg:justify-around lg:sticky pos-0">
+        <div id="bio" className="px-20 pr-40 pt-20 lg:pr-0 lg:max-w-xl">
           <h2 className="text-4xl">{charDisp ? charDisp[0]['name'].toUpperCase() : 'CharTitle'}</h2>
           <p>{charDisp ? charDisp[0]['height'] : 'CharHeight'}</p>
           <p>{charDisp ? charDisp[0]['weight'] : 'CharWeight'}</p>
@@ -22,26 +21,10 @@ export default function CharacterPage({ searchParams }: ICharacterPage) {
           <p>{charDisp ? charDisp[0]['notlike'] : 'CharNotlike'}</p>
           <p>{charDisp ? charDisp[0]['bio'] : 'CharBio'}</p>
         </div>
-        <div id="moves" className="p-24 static">
-          <div id="controller-options" className=" w-96 shadow-inner bg-gradient-to-r from-purple-950 to-indigo-900 p-3 px-4 flex justify-around rounded">
-            <div>
-              <button title='1' className="" type="button"><Image height={10} width={100} src="/moveBtn/logo-classic.png" alt="Classic"></Image></button>
-            </div>
-            <div>
-              <button title='2' type="button"><Image height={10} width={100} src="/moveBtn/logo-modern.png" alt="Modern"></Image></button>
-            </div>
-          </div>
-          <ul>
-            {
-              charDisp &&
-              charDisp[0]['moves'].map((move) => (
-                  <MoveElem
-                  key={move.id}
-                  move = {move}
-                  />
-              
-              ))}
-          </ul>
+        <div id="moves" className="pt-20 px-20 md:px-20 content-center ">
+          <MoveElem
+            char={charDisp}
+          />
         </div>
       </div>
     </div>
--- app/main/Move/move.tsx
+++ app/main/Move/move.tsx
@@ -1,38 +1,39 @@
+'use client';
+import { useState } from 'react';
+import Elem from './elem';
 
-
-export default function MoveElem(params:any) {
-  const move = params.move;
-  let first: string = '';
-  let second: string = '';
-  if (!move) return 'I aint got no moves man!';
-
-  if (move.classic) {
-    first = move.classic[1];
-    second = move.classic[0];
-    first ? first = first.toLowerCase() : first;
-    second ? second = second.toLowerCase() : second;
-  }
-
+export default function MoveElem(params: any) {
+  const { char } = params;
+  const [controlGen, setControlGen] = useState('classic');
+  const [controlMake, setControlMake] = useState('cap');
+  const movesObj = char[0].moves;
   return (
-    <li key={move.id}>
-      <div className="flex w-96 items-center p-3 bg-gradient-to-r from-purple-950 to-indigo-900 my-5 rounded shadow-inner">
-        <div
-          style={{ backgroundImage: `url("/moves/101.jpg")` }}
-          className="h-10 w-16 bg-cover bg-center rounded shadow-md"
-        ></div>
-        <div className="text-center pl-3">
-          <p>{move.name}</p>
-          <div className="flex px-5 py-1 items-center">
-            <span>
-              <div
-                className="h-6 w-6 z-10 bg-cover"
-                style={{ backgroundImage: `url("/moveBtn/icon_${first + second}.png")` }}
-              />
-            </span>
-            <p className="px-2 text-xs">{move.classic[0]}</p>
-          </div>
-        </div>
+    <div>
+      <div className="platform-select flex w-96 items-center justify-around bg-gradient-to-r from-purple-950 to-indigo-900 my-5 rounded shadow-inner">
+        <button className={controlMake === 'xbox' ? 'active' : '' } type="button" onClick={() => setControlMake('xbox')}>Xbox</button>
+        <button className={controlMake === 'play' ? 'active' : '' } type="button" onClick={() => setControlMake('play')}>Playstation</button>
+        <button className={controlMake === 'cap' ? 'active' : '' } type="button" onClick={() => setControlMake('cap')}>Capcom</button>
       </div>
-    </li >
-  )
+      <div className="control-select flex w-96 items-center justify-around bg-gradient-to-r from-purple-950 to-indigo-900 my-5 rounded shadow-inner">
+        <button title="Classic" className={controlGen === 'classic' ? 'active' : '' } type="button" onClick={() => setControlGen('classic')}><img title="i1" className="h-7" src="\movebtn\logo-classic.png"/></button>
+        <button title="Modern" className={controlGen === 'modern' ? 'active' : '' } type="button" onClick={() => setControlGen('modern')}><img title="i2" className="h-7" src="\movebtn\logo-modern.png"/></button>    
+      </div>
+      <div>
+       <ul>
+        {
+          movesObj &&
+          movesObj.map((move: any) => (
+            <li key={move.id}>
+              <Elem
+              move={move}
+              controlGen={controlGen}
+              controlMake={controlMake}
+              />
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+  </div>    
+)
 }
