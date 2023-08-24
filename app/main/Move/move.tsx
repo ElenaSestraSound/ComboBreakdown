f2bfb7e53c2dccdd2dae2bbc7a74967b8e9f507f
Chris Sorensen <chris_sorensen@live.com>
2023-08-24 18:30:28 +0200

    Feature/mainpagev9 (#52)
    Co-authored-by: Arto Lahtinen <artolle@pm.me>

--- app/main/Move/move.tsx
+++ app/main/Move/move.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useState } from 'react';
 import Elem from './elem';
+import Image from 'next/image';
 
 export default function MoveElem (params: any) {
   const { char } = params;
@@ -17,18 +18,24 @@ export default function MoveElem (params: any) {
       </div>
       <div className="control-select flex lg:w-96 items-center justify-around bg-gradient-to-r from-purple-950 to-indigo-900 my-5 rounded shadow-inner">
         <button title="Classic" className={controlGen === 'classic' ? 'active' : ''} type="button" onClick={() => setControlGen('classic')}>
-          <span
+          <Image
             title="i1"
-            className="h-4 w-48 z-10 p-0 mr-2 bg-cover bg-center"
-            style={{ backgroundImage: `url('/movebtn/logo/logoclassic.png')` }}>
-          </span>
+            height={60}
+            width={120}
+            src={'/movebtn/logo/logoclassic.png'}
+            className="z-10 p-0 mr-2"
+            alt="Classic"
+          />
         </button>
         <button title="Modern" className={controlGen === 'modern' ? 'active' : ''} type="button" onClick={() => setControlGen('modern')}>
-          <span
+          <Image
             title="i2"
-            className="h-4 w-48 z-10 p-0 mr-2 bg-cover bg-center"
-            style={{ backgroundImage: `url('/movebtn/logo/logomodern.png')` }}>
-          </span>
+            height={60}
+            width={120}
+            src={'/movebtn/logo/logomodern.png'}
+            className="z-10 p-0 mr-2"
+            alt="Modern"
+          />
         </button>
       </div>
       <div>
