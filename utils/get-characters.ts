0684e0a215698d5c2ccf2eeaab54c57ee1cd30c3
Arto Lahtinen <artolle@pm.me>
2023-08-28 09:58:03 +0200

    feat: increase revalidate for get characters

--- utils/get-characters.ts
+++ utils/get-characters.ts
@@ -1,14 +1,14 @@
 import { cache } from 'react';
 import { prisma } from '@/lib/prisma';
 
-export const revalidate = 60; // 3600 // revalidate at most every hour
+export const revalidate = 3600;
 
 export const getCharacters = cache(async () => {
   try {
     let characters = await prisma.character.findMany({
-    include: {
+      include: {
         moves: true
-    }
+      }
     });
     await prisma.$disconnect();
     return characters;
